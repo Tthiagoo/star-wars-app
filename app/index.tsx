3c1c76f6b92a862911af3c4e30af8f25c2f4e6d2
Thiago <thiago.pereira.araujosilva@gmail.com>
2024-06-19 10:01:53 -0300

    feat(package): add commitzen

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { StatusBar, Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="dark flex-1 items-center justify-center">
-      <Text>Open up App.js to start workiffng on your app!</Text>
+      <Text>Open up App.js to starttt workiffng on your app!</Text>
     </View>
   );
 }
